f6e459625d8afdf960e0952ee36fe86cf68e7e13
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-07-13 17:41:46 +0000

    Completed javascript-loops-practice exercise

---
+++ javascript-loops-practice/get-student-names.js
@@ -0,0 +1,8 @@
+/* exported getStudentNames */
+function getStudentNames(students) {
+  const result = [];
+  for (let i = 0; i < students.length; i++) {
+    result.push(students[i].name);
+  }
+  return result;
+}
