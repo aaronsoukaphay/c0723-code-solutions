a5e1dea31087ec844353f0f45c0da87703a5a57c
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-08-29 18:43:36 +0000

    created ToggleButton.css file and replaced button within button with p element

--- react-toggle-switch-component/my-app/src/ToggleButton.jsx
+++ react-toggle-switch-component/my-app/src/ToggleButton.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './ToggleButton.css';
 
 export default function ToggleButton({ label }) {
   const [isOn, setIsOn] = useState(true);
@@ -9,22 +10,22 @@ export default function ToggleButton({ label }) {
   if (isOn) {
     label = 'ON';
     bgColor = 'green';
-    position = 1;
+    position = 5;
   } else {
     label = 'OFF ';
     bgColor = 'red';
-    position = -31;
+    position = -35;
   }
 
   return (
-    <>
+    <div className="row">
       <button
         onClick={() => setIsOn(!isOn)}
         style={{ backgroundColor: bgColor }}
         className="toggle-switch">
-        <button className="slider" style={{ translate: position }}></button>
+        <p className="slider" style={{ translate: position }}></p>
       </button>
       <p className="state-label">{label}</p>
-    </>
+    </div>
   );
 }
