7b9000f399a651ecae67ccdcc9816e73c4764c32
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-09-03 23:33:15 +0000

    SearchComponent to be one file

--- react-search-component/my-app/src/SearchComponent.jsx
+++ react-search-component/my-app/src/SearchComponent.jsx
@@ -1,21 +1,36 @@
-import SearchBar from './SearchBar.jsx';
-import SearchResults from './SearchResults.jsx';
 import { useState } from 'react';
 
 export default function SearchComponent({ list }) {
-  const [typed, setTyped] = useState('');
+  const [search, setSearch] = useState('');
 
   function filter() {
-    const filtered = list.filter((statement) =>
-      statement.toLowerCase().includes(typed.toLowerCase())
+    const filtered = list.filter((item) =>
+      item.toLowerCase().includes(search.toLowerCase())
     );
     return filtered.length === 0 ? ['No items match the filter.'] : filtered;
   }
 
   return (
     <>
-      <SearchBar value={typed} onChange={(e) => setTyped(e.target.value)} />
-      <SearchResults filtered={filter()} />
+      <SearchBar
+        search={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <SearchItems list={list} filtered={filter()} />
     </>
+  );
+}
+
+function SearchBar({ search, onChange }) {
+  return <input value={search} onChange={onChange} placeholder="search" />;
+}
+
+function SearchItems({ filtered }) {
+  return (
+    <ul>
+      {filtered.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
   );
 }
