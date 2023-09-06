758ed557883eaf44618735e128109cbca8be4a11
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-09-06 22:25:02 +0000

    added default message and removed unnecessary code

--- node-module-system/calculate.js
+++ node-module-system/calculate.js
@@ -3,13 +3,13 @@ import subtract from './subtract.js';
 import multiply from './multiply.js';
 import divide from './divide.js';
 
-process.argv.forEach((val, index) => {
-  val === 'plus' &&
-    console.log('result:', add(process.argv[2], process.argv[4]));
-  val === 'minus' &&
-    console.log('result:', subtract(process.argv[2], process.argv[4]));
-  val === 'times' &&
-    console.log('result:', multiply(process.argv[2], process.argv[4]));
-  val === 'over' &&
-    console.log('result:', divide(process.argv[2], process.argv[4]));
-});
+const keywords = ['plus', 'minus', 'times', 'over'];
+
+const arg1 = Number(process.argv[2]);
+const arg2 = Number(process.argv[4]);
+
+process.argv[3] === 'plus' && console.log('result:', add(arg1, arg2));
+process.argv[3] === 'minus' && console.log('result:', subtract(arg1, arg2));
+process.argv[3] === 'times' && console.log('result:', multiply(arg1, arg2));
+process.argv[3] === 'over' && console.log('result:', divide(arg1, arg2));
+!keywords.includes(process.argv[3]) && console.log('invalid operation');
