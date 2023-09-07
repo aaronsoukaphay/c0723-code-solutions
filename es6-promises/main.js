91c4b52f5c64c51ffd2c22c3c689d89c23a3f934
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-09-07 20:49:20 +0000

    made small adjustments to main.js

--- es6-promises/main.js
+++ es6-promises/main.js
@@ -1,9 +1,9 @@
 import takeAChance from './take-a-chance.js';
 
-const p1 = takeAChance('Aaron');
-
-p1.then((value) => {
-  console.log(value);
-}).catch((e) => {
-  console.error(e.message);
-});
+takeAChance('Aaron')
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
