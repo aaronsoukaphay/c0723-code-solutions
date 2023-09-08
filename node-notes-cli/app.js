b3388790c78ec420d712894f1bd247bdf2edb547
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-09-08 23:14:19 +0000

    cleaned up code

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,34 +1,33 @@
 import { readFile, writeFile } from 'node:fs/promises';
 
 try {
-  const content = await readFile('data.json', 'utf8');
+  const content = await readFile('data.json');
   const parsedJSON = JSON.parse(content);
   for (const [key, value] of Object.entries(parsedJSON.notes)) {
     process.argv[2] === 'read' && console.log(`${key}: ${value}`);
   }
 
-  const [keyword, note] = process.argv.slice(2);
-  if (keyword === 'create') {
-    parsedJSON.notes[parsedJSON.nextId] = note;
+  const [createKeyword, createNote] = process.argv.slice(2);
+  if (createKeyword === 'create') {
+    parsedJSON.notes[parsedJSON.nextId] = createNote;
     parsedJSON.nextId++;
     const stringifyJSON = JSON.stringify(parsedJSON, null, 2);
-    writeFile('data.json', stringifyJSON);
+    await writeFile('data.json', stringifyJSON);
   }
 
-  const [update, selectedId, message] = process.argv.slice(2);
-  if (update === 'update') {
-    parsedJSON.notes[selectedId] = message;
+  const [updateKeyword, updateId, updateNote] = process.argv.slice(2);
+  if (updateKeyword === 'update') {
+    parsedJSON.notes[updateId] = updateNote;
     const stringifyJSON = JSON.stringify(parsedJSON, null, 2);
-    writeFile('data.json', stringifyJSON);
+    await writeFile('data.json', stringifyJSON);
   }
 
-  const [deletekeyword, deletedId] = process.argv.slice(2);
+  const [deletekeyword, deleteId] = process.argv.slice(2);
   if (deletekeyword === 'delete') {
-    delete parsedJSON.notes[deletedId];
+    delete parsedJSON.notes[deleteId];
     const stringifyJSON = JSON.stringify(parsedJSON, null, 2);
-    writeFile('data.json', stringifyJSON);
+    await writeFile('data.json', stringifyJSON);
   }
-  // node app.js delete 5
 } catch (error) {
   console.log(error.message);
 }
