61105d0f7087a54dbf0ac207517a64966b5bdd43
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-09-12 16:12:14 +0000

    Completed express-delete exercise

---
+++ express-delete/server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92,
+  },
+};
+
+const app = express();
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray = [];
+  for (const id in grades) {
+    gradesArray.push(grades[id]);
+  }
+  res.json(gradesArray);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  const gradeId = req.params.id;
+  if (grades[gradeId]) {
+    delete grades[gradeId];
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080!');
+});
